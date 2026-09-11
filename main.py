d8b1238726c448e90611564cd1a0a17b35e56229
DiegoBoyBau <diegoboyaca2012@hotmail.com>
2026-09-11 15:07:20 -0500

    Calculadora compleja

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+def addmultiplenumbers(numbers):
+   return sum(numbers)
+def multiplymultiplenumbers(numbers):
+    result = 1
+    for num in numbers:
+        result = result * num
+    return result
+def isitaninteger(num):
+    return isinstance(num, int)
+def isiteven(num):
+    return isitaninteger(num) and num % 2 == 0
 def main():
   print("Hello learners!")
+  print("Suma:", addmultiplenumbers([1, 2, 3, 4]))
+  print("Multiplicación:", multiplymultiplenumbers([1, 2, 3, 4]))
+  print("¿Es entero 5?:", isitaninteger(5))
+  print("¿Es par 4?:", isiteven(4))
 
 if __name__=="__main__":
   main()
